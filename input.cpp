3777bd0f6e561281a1851bc3162b27b8103ec190
Aryan Singh <aryansinghyu5@gmail.com>
2025-10-18 15:28:50 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter firts number:";
+    cin>>a;
+    cout<<"Enter second number:";
+    cin>>b;
+    cout<<"a+b:"<<a+b<<endl;
+    cout<<"a-b:"<<a-b<<endl;
+    cout<<"a*b:"<<a*b<<endl;
+    cout<<"a/b:"<<(float)a/b;
+    return 0;
+}
