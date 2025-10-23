24b1bf57021720bf70dfe11cbf5cf18e1d323254
Aryan Singh <aryansinghyu5@gmail.com>
2025-10-24 02:45:50 +0530

    Add files via upload
    I have written a basic if-else code that helps to find whether the number is even or odd.

---
+++ condition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"Given number is Even";
+    }
+    else
+    {
+        cout<<"Given number isn't even";
+    }
+    return 0;
+}
