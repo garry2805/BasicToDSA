1b9ac673a60215834dbafaf6934a1411dc65508f
Aryan Singh <aryansinghyu5@gmail.com>
2025-10-24 00:55:41 +0530

    Add files via upload
    Adding Comment in the program is a very good habit, it helps the programmer to understand the functioning of the program

---
+++ comment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // double slash is used for single comment
+    cout<<"// use for single line comment"<<endl;
+    /*
+    slash and the star
+    is used for multiline 
+    comment.Comments helps the programmer to understand the specific part in the  program.
+    */
+   cout<<"/* */ is used for multiline comments";
+    return 0;
+}
