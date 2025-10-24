41026e138c2f78b63ee746a6a3eb440002d70e50
Aryan Singh <aryansinghyu5@gmail.com>
2025-10-24 23:17:22 +0530

    Add files via upload
    My this code helps you to find the area and perimeter of the rectangle.

---
+++ rect.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length,breadth;
+    cout<<"Enter the Length:";
+    cin>>length;
+    cout<<"Enter the breadth";
+    cin>>breadth;
+    cout<<"perimeter of rectangle:"<< 2*(length+breadth)<<endl;
+    cout<<"area of rectangle:"<<length*breadth;
+    
+    return 0;
+}
