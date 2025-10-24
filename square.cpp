7513c90c571ffce1df742a9e905f2ff775e1a91a
Aryan Singh <aryansinghyu5@gmail.com>
2025-10-24 23:24:40 +0530

    Add files via upload
    My this program help you to find the area and perimeter of Square.

---
+++ square.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int side;
+    cout<<"Enter the side:";
+    cin>>side;
+    cout<<"Perimeter of square:"<< 4*side<<endl;
+    cout<<"Area of square:"<<side*side;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
