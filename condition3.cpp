224cc01c47fac66bbc552b816332e246b873f16a
Aryan Singh <aryansinghyu5@gmail.com>
2025-10-24 23:07:30 +0530

    Add files via upload
    find positive or negative of a number using ternary operations

---
+++ condition3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number :" ;
+    cin>>n;
+  string ch =(n >= 1 ? "positive":"negative");
+  cout<< ch;
+  
+    return 0;
+} 
