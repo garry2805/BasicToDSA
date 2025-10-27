d0daa418df038584eed24c5eac55934d79fd96d7
Aryan Singh <aryansinghyu5@gmail.com>
2025-10-28 01:29:08 +0530

    Add files via upload
    in this code,I have written a code to print sum of number from 1 to N.

---
+++ loopsum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int sum=0;
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+        
+    }
+    cout<<sum;
+    
+    return 0;
+}
